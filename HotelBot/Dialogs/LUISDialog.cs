ad8892094189bab4bbd4abde963ea33ffafafc60
Lee Borlace <lee.borlace@ignia.com.au>
2017-07-30 13:01:13 +0800

    Hook up to FB

--- HotelBot/Dialogs/LUISDialog.cs
+++ HotelBot/Dialogs/LUISDialog.cs
@@ -1,8 +1,10 @@
 ﻿using HotelBot.Models;
+using HotelBot.Models.FacebookModels;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.FormFlow;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,9 @@ namespace HotelBot.Dialogs
     public class LUISDialog : LuisDialog<RoomReservation>
     {
         private readonly BuildFormDelegate<RoomReservation> ReserveRoom;
+
+        [field: NonSerialized()]
+        protected Activity _message;
 
         public LUISDialog(BuildFormDelegate<RoomReservation> reserveRoom)
         {
@@ -51,7 +56,43 @@ namespace HotelBot.Dialogs
                 var value = entity.Entity.ToLower();
                 if (value == "pool" || value == "gym" || value == "wifi" || value == "towels")
                 {
-                    await context.PostAsync("Yes we have that!");
+                    //await context.PostAsync("Yes we have that!");
+
+                    Activity replyMessage = _message.CreateReply();
+                    var facebookMessage = new FacebookMessage();
+                    facebookMessage.notification_type = "NO_PUSH";
+                    facebookMessage.attachment = new FacebookAttachment();
+                    facebookMessage.attachment.type = "template";
+                    facebookMessage.attachment.payload = new FacebookPayload();
+                    facebookMessage.attachment.payload.template_type = "generic";
+
+
+                    var amenitiy = new FacebookElement();
+                    amenitiy.subtitle = "Yes we have that!";
+                    amenitiy.title = value;
+
+                    switch (value)
+                    {
+                        case "pool":
+                            amenitiy.image_url = "http://www.girltweetsworld.com/wp-content/uploads/2012/02/P1000180.jpg";
+                            break;
+                        case "gym":
+                            amenitiy.image_url = "https://s-media-cache-ak0.pinimg.com/originals/cb/c9/4a/cbc94af79da9e334a8555e850da136f4.jpg";
+                            break;
+                        case "wifi":
+                            amenitiy.image_url = "http://media.idownloadblog.com/wp-content/uploads/2016/02/wifi-icon.png";
+                            break;
+                        case "towels":
+                            amenitiy.image_url = "http://www.prabhutextile.com/images/bath_towel_1.jpg";
+                            break;
+                        default:
+                            break;
+                    }
+                    facebookMessage.attachment.payload.elements = new FacebookElement[] { amenitiy };
+                    replyMessage.ChannelData = facebookMessage;
+
+                    await context.PostAsync(replyMessage);
+
                     context.Wait(MessageReceived);
                     return;
                 }
@@ -62,10 +103,16 @@ namespace HotelBot.Dialogs
                     return;
                 }
             }
-
             await context.PostAsync("I'm sorry we don't have that.");
             context.Wait(MessageReceived);
             return;
+        }
+
+
+        protected override async Task MessageReceived(IDialogContext context, IAwaitable<IMessageActivity> item)
+        {
+            _message = (Activity)await item;
+            await base.MessageReceived(context, item);
         }
 
 
